5944deae8da1d65ea4b09df3e3f6772aa9226fba
pforderique <pforderique@gmail.com>
2025-07-04 08:13:39 -0700

    Setup main package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="screener",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "npyscreen>=4.10.5",
+        "requests>=2.32.3",
+        "python-dotenv>=1.1.0",
+        "setuptools>=68.0.0"
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=8.4.1",
+        ],
+    },
+)
