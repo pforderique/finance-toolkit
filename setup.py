464a2cd0a6f59ad354da42a14016c6bf59167fa6
pforderique <pforderique@gmail.com>
2025-07-12 21:34:15 -0700

    update setup to include validators

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
-from setuptools import setup, find_packages
+"""Setup script for the screener package."""
 
-setup(
+import setuptools
+
+setuptools.setup(
     name="screener",
     version="0.1.0",
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     install_requires=[
         "npyscreen>=4.10.5",
         "pydantic>=2.11.7",
         "python-dotenv>=1.1.0",
         "redis>=6.2.0",
         "requests>=2.32.3",
-        "setuptools>=68.0.0"
+        "setuptools>=68.0.0",
+        "validators>=0.35.0",
     ],
     extras_require={
         "dev": [
